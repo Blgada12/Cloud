b1928795e02479b1e8afc4ef75593653f55b78b6
blgada12 <blgada12@naver.com>
2017-04-13 07:34:46 +0900

    0.0.10

---
+++ test2/app/src/main/java/com/example/smart/test2/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.smart.test2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button1 = (Button) findViewById(R.id.button);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "버튼눌림", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
