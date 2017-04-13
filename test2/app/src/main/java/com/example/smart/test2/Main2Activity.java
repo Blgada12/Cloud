032af92f58d8eccd60d15847d53a93fab8dfc1ae
blgada12 <blgada12@naver.com>
2017-04-13 11:38:45 +0900

    0.0.11

---
+++ test2/app/src/main/java/com/example/smart/test2/Main2Activity.java
@@ -0,0 +1,25 @@
+package com.example.smart.test2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Button but1 = (Button) findViewById(R.id.button1);
+        but1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent pagemove = new Intent(Main2Activity.this, MainActivity.class);
+                startActivity(pagemove);
+            }
+        });
+    }
+}
