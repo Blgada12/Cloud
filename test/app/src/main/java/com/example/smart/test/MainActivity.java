44829efb9917d07d0af5d2bee6e91ecf87ed9a38
blgada12 <blgada12@naver.com>
2017-04-13 17:05:49 +0900

    0.0.13

--- test/app/src/main/java/com/example/smart/test/MainActivity.java
+++ test/app/src/main/java/com/example/smart/test/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.smart.test;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn1 = (Button) findViewById(R.id.button1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent GJ = new Intent(Intent.ACTION_VIEW, Uri.parse("http://blgada12.blog.me"));
+                startActivity(GJ);
+            }
+        });
+
+        Button btn2 = (Button) findViewById(R.id.button2);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
     }
 }
